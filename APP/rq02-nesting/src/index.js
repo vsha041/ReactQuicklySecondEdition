0bb204fb2989d1b7f1bd83650255b2192403f0d6
Varun Sharma <zX1102987@wowcorp.com.au>
2025-04-05 15:32:51 +1300

    rq02-siblings-div

--- APP/rq02-nesting/src/index.js
+++ APP/rq02-nesting/src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const world = React.createElement("em", null, "world");
-const title = React.createElement("h1", null, ["Hello", world, "!"]);
+const title = React.createElement("h1", null, "Hello World");
+const link = React.createElement("a", {href: "//react.dev"}, "Read more");
+const group = React.createElement("div", null, title, link);
 const domElement = document.getElementById("root");
-const root = ReactDOM.createRoot(domElement);
-root.render(title);
+ReactDOM.createRoot(domElement).render(group);
