59389e465ecc8004e9ad4d0735ac18ccaf77175e
Varun Sharma <zX1102987@wowcorp.com.au>
2025-04-05 15:35:35 +1300

    rq02-siblings-fragment

--- APP/rq02-nesting/src/index.js
+++ APP/rq02-nesting/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 
 const title = React.createElement("h1", null, "Hello World");
 const link = React.createElement("a", {href: "//react.dev"}, "Read more");
-const group = React.createElement("div", null, title, link);
+
+const group = React.createElement(React.Fragment, null, title, link);
+
 const domElement = document.getElementById("root");
 ReactDOM.createRoot(domElement).render(group);
